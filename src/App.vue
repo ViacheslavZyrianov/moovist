f4dc3c670d0efc231f0ef8b1a524552b42092eda
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2020-03-31 01:18:59 +0300

    add more styles for homepage

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
 </template>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100,300;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
 
   * {
     box-sizing: border-box;
@@ -18,8 +19,17 @@
   .moovist {
     display: flex;
     font-family: 'Roboto', serif;
+    font-weight: 100;
+    color: #292929;
     min-height: 100vh;
     padding: 24px;
+  }
+
+  .container {
+    width: 70vw;
+    min-width: 960px;
+    display: flex;
+    flex-direction: column;
   }
 
   .page {
@@ -27,19 +37,23 @@
   }
 
   .input {
+    font-weight: 100;
     border: 1px solid #afafaf;
     border-radius: 4px;
     background-color: transparent;
   }
 
   .button {
-    padding: 8px 16px;
+    font-weight: 100;
+    font-size: 24px;
+    color: #fff;
+    padding: 16px 24px;
     border: 0;
     border-radius: 4px;
     background-color: #20639b;
-    color: #fff;
     cursor: pointer;
     transition: background-color 0.3s;
+    user-select: none;
 
     &:hover {
       background-color: lighten(#20639b, 16);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,69 @@
 <template>
   <div :class="pageHomeClassList">
-    <label
-      for="movieSearch"
-      class="page_home__title"
-    >
-      MOOVIST
-    </label>
-    <form
-      @submit.prevent="onSearchMovies"
-      class="movie-search__form"
-    >
-      <input
-        v-model="movieSearch"
-        type="text"
-        placeholder="Search for a movie..."
-        class="input form__input"
+    <div class="container">
+      <div class="page_home__title">
+        moovist
+      </div>
+      <form
+        @submit.prevent="onSearchMovies"
+        class="movie-search__form"
       >
-      <button
-        :disabled="isSubmitButtonDisabled"
-        type="submit"
-        class="button form__button"
-      >
-        Search
-      </button>
-    </form>
-    <template v-if="isMovieListVisible">
-      <div class="movie-search__controls">
-        <div class="movie-search__total">
-          Found {{ movieListTotalCount }} movies
-        </div>
-        <div class="movie-search__filter-list">
-          <div
-            v-for="filterItem in filterList"
-            :key="filterItem.id"
-            class="filter-list__filter-item"
-          >
-            <label
-              :for="filterItem.id"
-              class="filter-item__label"
+        <input
+          v-model="movieSearch"
+          type="text"
+          placeholder="Search for a movie..."
+          class="input form__input"
+        >
+        <button
+          :disabled="isSubmitButtonDisabled"
+          type="submit"
+          class="button form__button"
+        >
+          Search
+        </button>
+      </form>
+      <template v-if="isMovieListVisible">
+        <div class="movie-search__controls">
+          <div class="movie-search__total">
+            Found {{ movieListTotalCount }} movies
+          </div>
+          <div class="movie-search__filter-list">
+            <div
+              v-for="filterItem in filterList"
+              :key="filterItem.id"
+              class="filter-list__filter-item"
             >
-              {{ filterItem.label }}
-            </label>
-            <input
-              :id="filterItem.id"
-              :name="filterItem.name"
-              :value="filterItem.value"
-              v-model="selectedFilter"
-              type="radio"
-            >
+              <label
+                :for="filterItem.id"
+                class="filter-item__label"
+              >
+                {{ filterItem.label }}
+              </label>
+              <input
+                :id="filterItem.id"
+                :name="filterItem.name"
+                :value="filterItem.value"
+                v-model="selectedFilter"
+                type="radio"
+              >
+            </div>
           </div>
         </div>
+        <MovieList
+          :movie-list="movieListFiltered"
+          class="movie-search__movie-list"
+        />
+        <button
+          v-if="isButtonShowMoreMoviesVisible"
+          class="button"
+          @click="onFetchMoreMovies"
+        >
+          Show more movies
+        </button>
+      </template>
+      <div class="movie-search__error">
+        {{ movieListError }}
       </div>
-      <MovieList
-        :movie-list="movieListFiltered"
-        class="movie-search__movie-list"
-      />
-      <button
-        v-if="isButtonShowMoreMoviesVisible"
-        class="button"
-        @click="onFetchMoreMovies"
-      >
-        Show more movies
-      </button>
-    </template>
-    <div class="movie-search__error">
-      {{ movieListError }}
     </div>
   </div>
 </template>
@@ -168,22 +167,44 @@ export default {
 
     &-with-search-results {
       justify-content: flex-start;
+
+      .page_home__title {
+        margin-top: 0;
+      }
     }
     
     &__title {
-      font-size: 72px;
-      font-weight: 700;
+      font-family: 'Righteous';
+      font-size: 160px;
       text-align: center;
       margin-bottom: 24px;
+      margin-top: -184px;
+      user-select: none;
+      letter-spacing: normal;
+      animation: titleIntro linear 0.7s;
+
+      @keyframes titleIntro {
+        0% {
+          letter-spacing: -15px;
+          opacity: 0;
+        }
+        100% {
+          letter-spacing: normal;
+          opacity: 1;
+        }
+      }
     }
   }
   
   .movie-search {
     &__controls {
+      position: sticky;
+      top: 85px;
       width: 100%;
       display: flex;
       justify-content: space-between;
-      margin-bottom: 16px;
+      padding: 16px 0;
+      background-color: #fff;
     }
 
     &__filter-list {        
@@ -191,9 +212,24 @@ export default {
     }
 
     &__form {
+      position: sticky;
+      top: 0;
       display: flex;
       width: 100%;
-      margin-bottom: 32px;
+      padding-top: 24px;
+      background-color: #fff;
+      animation: formIntro linear 0.7s;
+
+      @keyframes formIntro {
+        0% {
+          transform: translateY(-50%);
+          opacity: 0;
+        }
+        100% {
+          transform: translateY(0);
+          opacity: 1;
+        }
+      }
     }
 
     &__movie-list {
